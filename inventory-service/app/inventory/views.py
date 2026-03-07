9f6d021fe1bd467285a63b74e1beb7da644a11fa
edddddr <endt6342@gmail.com>
2026-03-07 02:43:13 -0900

    feat: implemented stock reservation

--- inventory-service/app/inventory/views.py
+++ inventory-service/app/inventory/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .serializers import (
+    ReserveStockSerializer,
+    ReservationActionSerializer
+)
+
+from .services import InventoryService
+
+
+class ReserveStockView(APIView):
+
+    def post(self, request):
+
+        serializer = ReserveStockSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        reservation = InventoryService.reserve_stock(
+            order_id=serializer.validated_data["order_id"],
+            product_id=serializer.validated_data["product_id"],
+            quantity=serializer.validated_data["quantity"],
+        )
+
+        return Response(
+            {
+                "reservation_id": reservation.id,
+                "status": reservation.status
+            },
+            status=status.HTTP_201_CREATED
+        )
