2481fab2b4d73475daa318670624f312d93c004c
edddddr <endt6342@gmail.com>
2026-03-14 04:53:33 -0900

    feat: admin was implemted for two entity modles

--- payment-service/app/payments/admin.py
+++ payment-service/app/payments/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Payment, PaymentTransaction
 
-# Register your models here.
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "id",
+        "order_id",
+        "amount",
+        "currency",
+        "status",
+        "created_at",
+    )
+
+
+@admin.register(PaymentTransaction)
+class PaymentTransactionAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "transaction_id",
+        "provider",
+        "status",
+        "created_at",
+    )
